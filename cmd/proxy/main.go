19189bafcec02591e1eef8c945ce875cc5ef4d99
Oleksii Shvorak <aleksey.shvorak@gmail.com>
2025-12-03 22:23:46 +0200

    Implement basic config loading

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -4,9 +4,25 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"reverse-proxy/internal/config"
 )
 
 func main() {
+	if len(os.Args) < 2 {
+		log.Fatalf("Configuration file required.")
+	}
+
+	config, err := config.Load(os.Args[1])
+	if err != nil {
+		log.Fatalf("Failed to load config file %v", err)
+	}
+
+	log.Println("Service started with configuration:")
+	for _, r := range config.Routes {
+		log.Printf("Prefix %s, Upstream %s\n", r.Prefix, r.Upstream)
+	}
+
 	http.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "OK")
 	})
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,8 +1,32 @@
 package config
 
+import (
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Route struct {
+	Prefix   string `yaml:"prefix"`
+	Upstream string `yaml:"upstream"`
+}
+
 type Config struct {
+	Routes []Route `yaml:"routes"`
 }
 
 func Load(path string) (*Config, error) {
-	return &Config{}, nil
+	data, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatalf("Error reading file: %v", err)
+	}
+
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		log.Fatalf("Error marshaling yaml config: %v", err)
+	}
+
+	return &config, nil
 }
